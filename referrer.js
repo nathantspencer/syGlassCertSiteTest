c7e9b238cd04d748e2cdae2bd54acc4ef8054c8d
Nathan Spencer <nathantspencer@gmail.com>
2025-08-21 16:45:49 -0400

    Another test

--- referrer.js
+++ referrer.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 	const slashSplits = location.href.split('/');
 	const slashSplitsLength = slashSplits.length;
-	const requestedID = splashSplits[slashSplitsLength - 1].split('.')[0];
+	const requestedID = slashSplits[slashSplitsLength - 1].split('.')[0];
 	document.getElementById('badCert').innerHTML = requestedID;
 });
 
